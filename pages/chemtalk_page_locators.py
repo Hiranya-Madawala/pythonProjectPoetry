d71e07d4956064dc0bda8960e85ac55f7f923d65
hiran <hiranyamalshani28@gmail.com>
2024-10-01 23:01:52 -0400

    locators changed_XPATHS updated

--- pages/chemtalk_page_locators.py
+++ pages/chemtalk_page_locators.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 
 class ChemTalkPageLocators:
-    LOGIN_LINK = (By.ID, 'login-link')
-    FIRST_OPTION = (By.CSS_SELECTOR, '.first-option')
+    LOGIN_LINK = (By.XPATH, '//*[@id="b_results"]/li[1]/h2/a')
+    #FIRST_OPTION = (By.CSS_SELECTOR, '.first-option')
+    FIRST_OPTION = (By.XPATH, "//*[@id='menu-item-7150']/div/a")
     LOGIN_BUTTON = (By.ID, 'login-button')
     MANGANESE_ELEMENT = (By.CSS_SELECTOR, '.element-manganese')
 
